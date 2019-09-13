91763b85c04786570cb1c11c1b177625c7c0a071
Decimoseptimo <decimoseptimo@gmail.com>
2019-09-12 17:11:26 -0700

    Add pagination menu

---
+++ src/components/pagination.js
@@ -0,0 +1,70 @@
+import React from "react"
+import { Pagination as SUI_Pagination } from "semantic-ui-react"
+
+const Pagination = props => {
+  return (
+    <div className="paginationWrapper">
+      <SUI_Pagination {...props} />
+      <style jsx global>{`
+      .paginationWrapper {
+        display: flex;
+      }
+
+      .ui.pagination.menu {
+        margin: 0 auto;
+        margin-top: 3rem;
+      }
+
+      .ui.menu {
+        background: none;
+        box-shadow: none;
+        border: none;
+      }
+
+      .ui.pagination.menu .item {
+          font-weight: bold;
+          color: #96588a;
+      }
+
+      .ui.pagination.menu .active.item {
+        color: #96588a;
+      }
+
+      .ui.menu .item:before {
+        width: 0;
+      }
+
+      .ui.pagination.menu .item[type = "ellipsisItem"] {
+        padding: 0 !important;
+        min-width: auto;
+        opacity: .5;
+      }
+
+
+      .ui.menu a.item[type = "ellipsisItem"]:hover {
+        cursor: normal;
+        color: inherit;
+      }
+
+      .ui.pagination.menu .item[type = "ellipsisItem"]:hover {
+        background: none;
+        color: #96588a;
+        cursor: default;
+      }
+
+      .ui.pagination.menu .icon.item[type = "ellipsisItem"] i.icon {
+        font-size: .3rem;
+      }
+
+      a.active.item::after {
+        // content: "_";
+        // position: absolute;
+        // top: 1.5rem;
+        // left: 1.2rem;
+        // font-size: 1.4rem;
+      `}</style>
+    </div>
+  )
+}
+
+export default Pagination
--- src/templates/productIndex.js
+++ src/templates/productIndex.js
@@ -1,16 +1,36 @@
 import React from "react"
-import { Image, graphql } from "gatsby"
+import { Image, graphql, navigate } from "gatsby"
 
 import SEO from "../components/seo"
 import ProductGrid from "../components/product/productGrid"
+import Pagination from "../components/pagination"
+import { Icon } from "semantic-ui-react"
 
 const ProductIndex = props => {
   const products = props.data.allProductsJson.edges
+
+  const handlePageChange = (e, d) => {
+    d.activePage == 1 ? navigate(`/`) : navigate(`/${d.activePage}`)
+  }
 
   return (
     <>
       <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
       <ProductGrid products={products} />
+      <Pagination
+        activePage={props.pageContext.humanPageNumber}
+        ellipsisItem={{
+          content: <Icon name="circle" />,
+          icon: true,
+        }}
+        prevItem={{ content: <Icon name="angle left" />, icon: true }}
+        nextItem={{ content: <Icon name="angle right" />, icon: true }}
+        totalPages={props.pageContext.numberOfPages}
+        firstItem={null}
+        lastItem={null}
+        // ellipsisItem={null}
+        onPageChange={handlePageChange}
+      />
     </>
   )
 }
