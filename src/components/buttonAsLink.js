67badb0d26dd299ed1858733f27ae6e3e14f69ed
Miguel Valenzuela <decimoseptimo@gmail.com>
2021-03-10 11:00:12 -0800

    Fix color contrast ratio (update to devtoools suggested value)

--- src/components/buttonAsLink.js
+++ src/components/buttonAsLink.js
@@ -5,7 +5,7 @@ import css from "styled-jsx/css"
 const ButtonAsLink = (props) => {
   const { className, styles } = css.resolve`
     .buttonAsLink {
-      color: #4183c4;
+      color: #3c79b6;
       padding: 0;
     }
   `
